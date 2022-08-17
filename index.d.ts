fa04355694fd31a8127a0b64e6234fa4e08d6911
jiayou <tengsong.ts@antfin.com>
2022-08-17 12:00:01 +0800

    添加扩展interface

--- index.d.ts
+++ index.d.ts
@@ -67,5 +67,73 @@ export namespace AST {
     value: any;
   }
 }
+export namespace JAST {
+
+  type JValue = JObject | JArray | JLiteral;
+
+  interface JPosition {
+      line: number;
+      column: number;
+      offset: number;
+  }
+
+  interface JLocation {
+      start: JPosition;
+      end: JPosition;
+      source?: string | null;
+  }
+
+  interface JEsNode {
+    range: number[];
+    parent?: JNode;
+    children?: JNode[];
+  }
+
+  interface JNode extends JEsNode {
+      type: string;
+      loc?: JLocation;
+  }
+
+  interface JObject extends JNode {
+      type: "Object";
+      children: JProperty[];
+  }
+
+  interface JProperty extends JNode {
+      type: "Property";
+      key: JIdentifier;
+      value: JValue;
+  }
+
+  interface JIdentifier extends JNode {
+      type: "Identifier";
+      value: string;
+      raw: string;
+  }
+
+  interface JArray extends JNode {
+      type: "Array";
+      children: JValue[];
+  }
+
+  interface JLiteral extends JNode {
+      type: "Literal";
+      value: string | number | boolean | null;
+      raw: string;
+  }
+
+  /**
+   * 新增 Program
+   */
+  interface JProgram extends JNode {
+    type: 'Program';  //  必须带有 Program 类型节点
+    body: JNode[];    //  必须带有 body 属性，eslint-scope时会默认访问该属性
+    tokens: JToken[];
+    comments: JToken[];
+  }
+  interface JToken extends JNode {
+    value: string;
+  }
+}
 export declare function parse(input: string, settings?: AST.Options): AST.ValueNode | undefined;
 export declare function tokenize(input: string, settings?: AST.Options): AST.IToken[];
